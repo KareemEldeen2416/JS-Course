21e5750b0be70b8c3dcb44f4a0da107aefa12972
KareemEldeen Ahmed <kareemeldeenahmed@gmail.com>
2023-09-13 07:19:57 -0700

    First ten videos

---
+++ app.js
@@ -0,0 +1,19 @@
+//----Output to screen----//
+
+// window.alert("This messages is for you");
+// document.write("<h1>Created element by JS</h1>");
+
+
+
+
+//----Console methods, styling, and webAPI----//
+
+// console.log("Log");
+// console.error("Error");
+// console.table([ "Hamada" , "Semem"  ,"Zero" ]);
+// console.log("Normal %cmessage" , "color:blue;");
+
+
+
+
+//----ECMAScript----//
