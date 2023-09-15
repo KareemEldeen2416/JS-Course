c0e30bdd6b5d26830b08d62e85e3bc49e5094b79
KareemEldeen Ahmed <kareemeldeenahmed@gmail.com>
2023-09-14 20:10:32 -0700

    10 to 20

--- app.js
+++ app.js
@@ -17,3 +17,45 @@
 
 
 //----ECMAScript----//
+
+
+
+
+//----Variables declaration----//
+
+// var user = "KareemEldeen";
+// console.log(user);
+// hello.innerHTML = "Modified variable";
+
+
+// console.log("Ibda 'Academy'");
+// console.log('Ibda "Academy"');
+// console.log("Ibda \"Academy\"");
+
+
+
+
+//----Concatenation----//
+
+// let a = "I";
+// let b = "am";
+// let c = "comming";
+// console.log(`${a} ${b} ${c} in about ${3*5} minutes`);
+// console.log(2 ** 4);
+// console.log ( 2*2*2*2);
+console.log(+100);
+console.log(+"100");
+console.log(-100);
+console.log(-"-100");
+
+
+
+
+
+
+
+
+
+
+
+
